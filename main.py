897c988271c83a69f1649da7a58dfa3a94b5b98e
rus152 <77641121+rus152@users.noreply.github.com>
2024-09-06 19:56:40 +0400

    Исправлено логирование (патч 2)

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from threading import Thread
 
 class ChatManager:
     def __init__(self):
-        logger.info('Инициализируем пустой массив для хранения чатов')
+        logging.info('Инициализируем пустой массив для хранения чатов')
         self.chat_data = []
 
     def add_chat(self, chat, message, path):
-        logger.info('Добавляем новый элемент в массив')
+        logging.info('Добавляем новый элемент в массив')
         self.chat_data.append({
             "chat_id": chat,
             "message_id": message,
@@ -20,12 +20,12 @@ class ChatManager:
         return f"Добавлено: {chat}, {message}, {path}"
 
     def remove_chat(self, chat):
-        logger.info('Удаляем элемент по имени чата')
+        logging.info('Удаляем элемент по имени чата')
         self.chat_data = [item for item in self.chat_data if item["chat_id"] != chat]
         return f"Удалено все сообщения из чата: {chat}"
 
     def display_chats(self):
-        logger.info(' Вывод всех данных в массиве')
+        logging.info('Вывод всех данных в массиве')
         if not self.chat_data:
             return "Чаты отсутствуют"
         else:
@@ -33,7 +33,7 @@ class ChatManager:
                 return f"Чат: {item['chat_id']}, Сообщение: {item['message_id']}, Путь: {item['path']}"
 
     def get_first_chat(self):
-        logger.info(f'{datetime.now().strftime("%H:%M:%S")} Вывод первого чата в массиве')
+        logging.info(f'Вывод первого чата в массиве')
         if self.chat_data:
             first_chat = self.chat_data[0]
             return f"Первый чат: {first_chat['chat_id']}, Сообщение: {first_chat['message_id']}, Путь: {first_chat['path']}"
@@ -41,7 +41,7 @@ class ChatManager:
             return "Массив пуст"
 
     def count_chats(self):
-        logger.info('Вывод количества чатов')
+        logging.info('Вывод количества чатов')
         return f"Количество чатов: {len(self.chat_data)}"
 
 def setup_logging(filename: str) -> None:
@@ -51,7 +51,6 @@ def setup_logging(filename: str) -> None:
     :param filename: Имя файла для записи логов
     :return: None
     """
-    global logger
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)  # Устанавливаем уровень логирования
 
@@ -76,33 +75,32 @@ def init():
     global API_TOKEN
     global chat_manager
 
-    logging.basicConfig(filename='myapp.log', level=logging.INFO)
-    logger.info(f'{datetime.now().strftime("%H:%M:%S")} Начало логирования')
+    logging.info('Начало логирования')
 
     chat_manager = ChatManager()
 
     load_dotenv()
     API_TOKEN = os.getenv('TOKEN')
-    logger.info(f'{datetime.now().strftime("%H:%M:%S")} Токен получен')
+    logging.info('Токен получен')
 
     VOICE_FOLDER = 'voice_messages'
     VIDEO_NOTE = 'video_notes'
 
     if not os.path.exists(VOICE_FOLDER):
         os.makedirs(VOICE_FOLDER)
-        logger.info(f'{datetime.now().strftime("%H:%M:%S")} Папка для видео сообщений создана')
+        logging.info('Папка для видео сообщений создана')
     if not os.path.exists(VIDEO_NOTE):
         os.makedirs(VIDEO_NOTE)
-        logger.info('Папка для голосовых сообщений создана')
-    logger.info(f'{datetime.now().strftime("%H:%M:%S")} Инициализация закончена')
+        logging.info('Папка для голосовых сообщений создана')
+    logging.info('Инициализация закончена')
 
 def main():
-    logger.info(f'{datetime.now().strftime("%H:%M:%S")} Запуск бота')
+    logging.info('Запуск бота')
     bot = telebot.TeleBot(API_TOKEN)
 
     @bot.message_handler(content_types=['voice'])
     def handle_voice(message):
-        logger.info(f'{datetime.now().strftime("%H:%M:%S")} Получено голосовое сообщение')
+        logging.info('Получено голосовое сообщение')
         pass
 
     @bot.message_handler(commands=['add'])
